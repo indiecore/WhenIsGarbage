1a3d1f6abd091082a1f47bbe5ade083f601493c5
Kris <kris@krisgee.com>
2013-08-24 14:03:00 -0300

    Skeleton flask website

---
+++ runserver.py
@@ -0,0 +1,12 @@
+import os
+from WhenIsGarbage import app
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+
+    if os.environ.get('FOREMAN') == 'True':
+    	app.debug = True
+    	app.run()
+    else:
+    	app.run(host='0.0.0.0',port=port)
+
